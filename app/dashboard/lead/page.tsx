ab74daa2504368c743fb4dbee386438992a07a35
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-02-08 10:46:42 -0500

    Add user list and fetch functionality

--- app/dashboard/lead/page.tsx
+++ app/dashboard/lead/page.tsx
@@ -1,12 +1,38 @@
+"use client";
+import { useEffect, useState } from 'react';
 import Search from '@/app/ui/dashboard/search/search';
-import styles from '../../ui/dashboard/lead/lead.module.css'
+import styles from '../../ui/dashboard/lead/lead.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 import { MdAdd } from 'react-icons/md';
 
-
+// Definir la interfaz para el objeto de usuario
+interface User {
+    id: number;
+    email: string;
+    first_name: string;
+    last_name: string;
+    avatar: string;
+}
 
 const LeadPage = () => {
+    const [users, setUsers] = useState<User[]>([]);
+
+    // Función para obtener la lista de usuarios
+    useEffect(() => {
+        async function fetchUsers() {
+            try {
+                const res = await fetch('https://reqres.in/api/users');
+                const data = await res.json();
+                setUsers(data.data);
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        }
+
+        fetchUsers();
+    }, []);
+
     return (
         <div className={styles.container}>
             <div className={styles.top}>
@@ -22,41 +48,39 @@ const LeadPage = () => {
             <table className={styles.table}>
                 <thead>
                     <tr>
-                        <td>
-                            Photo
-                        </td>
-                        <td>
-                            Last Name
-                        </td>
-                        <td>
-                            Phone
-                        </td>
-                        <td>
-                            Email Address
-                        </td>
-                        <td>
-                            Lead Status
-                        </td>
-                        <td>
-                            Date Time Registration
-                        </td>
-                        <td></td>
+                        <th>Photo</th>
+                        <th>Last Name</th>
+                        <th>Phone</th>
+                        <th>Email Address</th>
+                        <th>Lead Status</th>
+                        <th>Date Time Registration</th>
+                        <th></th>
                     </tr>
                 </thead>
 
                 <tbody>
-                    <tr>
-                        <td>
-                            <div className={styles.user}>
-                                <Image
-                                    src={
-                                        // avatar
-                                    }
-                                 alt='user photo' width={40} height={40} className={styles.userImage}/>
-                                
-                            </div>
-                        </td>
-                    </tr>
+                    {users.map((user) => (
+                        <tr key={user.id}>
+                            <td>
+                                <div className={styles.user}>
+                                    <Image src={user.avatar} alt={user.first_name} width={40} height={40} className={styles.userImage} />
+                                </div>
+                            </td>
+                            <td>{user.last_name}</td>
+                            <td>987654321</td>
+                            <td>{user.email}</td>
+                            <td>In Progress</td>
+                            <td>2022-01-01</td>
+                            <td>
+                                <div className={styles.buttons}>
+                                    <Link href=''>
+                                        <button className={`${styles.button} ${styles.view}`}>View</button>
+                                    </Link>
+                                    <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
---
+++ app/ui/dashboard/pagination/pagination.tsx
@@ -0,0 +1,11 @@
+import styles from './pagination.module.scss'
+
+const Pagination = () => {
+    return (
+        <div className={styles.container}>
+            Pagination
+        </div>
+    )
+}
+
+export default Pagination
